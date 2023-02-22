9b4a703eb626cd8cfbd0683c66bd21c75e14dca8
Yiddies <chrislicks104@gmail.com>
2023-02-21 20:28:34 -0500

    Redid entire thing

---
+++ script.js
@@ -0,0 +1,45 @@
+function createBoard(size) {
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div => div.remove()))
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div')
+        square.addEventListener('mouseover', colorSquare)
+        square.className = "boxes"
+        board.insertAdjacentElement("beforeend", square)
+    }
+}
+
+createBoard(16)
+
+function chooseSize(input) {
+    if (input >= 2 && input <= 100) {
+        createBoard(input)
+    } else
+        console.log("ERROR")
+}
+
+function colorSquare(choice) {
+    this.style.background = choice
+}
+
+function resetBoard() {
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll('div');
+
+    squares.forEach((div => div.style.background = "white"))
+}
+
+function chooseColor(input) {
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll('div');
+
+    color = input
+    colorSquare(color)
+
+}
